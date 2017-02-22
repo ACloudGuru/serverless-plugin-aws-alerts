0fb911c6313f8827206aec52262b0774187e3c1f
Jonathan Dietrich <jdietrich@desire2learn.com>
2017-02-22 08:42:14 -0500

    restoring eslint and spacing in definitions

--- src/defaults/definitions.js
+++ src/defaults/definitions.js
@@ -3,40 +3,40 @@
 const lambdaNamespace = 'AWS/Lambda';
 
 module.exports = {
-	functionInvocations: {
-		namespace: lambdaNamespace,
-		metric: 'Invocations',
-		threshold: 100,
-		statistic: 'Sum',
-		period: 60,
-		evaluationPeriods: 1,
-		comparisonOperator: 'GreaterThanThreshold',
-	},
-	functionErrors: {
-		namespace: lambdaNamespace,
-		metric: 'Errors',
-		threshold: 10,
-		statistic: 'Maximum',
-		period: 60,
-		evaluationPeriods: 1,
-		comparisonOperator: 'GreaterThanThreshold',
-	},
-	functionDuration: {
-		namespace: lambdaNamespace,
-		metric: 'Duration',
-		threshold: 500,
-		statistic: 'Maximum',
-		period: 60,
-		evaluationPeriods: 1,
-		comparisonOperator: 'GreaterThanThreshold',
-	},
-	functionThrottles: {
-		namespace: lambdaNamespace,
-		metric: 'Throttles',
-		threshold: 50,
-		statistic: 'Sum',
-		period: 60,
-		evaluationPeriods: 1,
-		comparisonOperator: 'GreaterThanThreshold',
-	}
+  functionInvocations: {
+    namespace: lambdaNamespace,
+    metric: 'Invocations',
+    threshold: 100,
+    statistic: 'Sum',
+    period: 60,
+    evaluationPeriods: 1,
+    comparisonOperator: 'GreaterThanThreshold',
+  },
+  functionErrors: {
+    namespace: lambdaNamespace,
+    metric: 'Errors',
+    threshold: 10,
+    statistic: 'Maximum',
+    period: 60,
+    evaluationPeriods: 1,
+    comparisonOperator: 'GreaterThanThreshold',
+  },
+  functionDuration: {
+    namespace: lambdaNamespace,
+    metric: 'Duration',
+    threshold: 500,
+    statistic: 'Maximum',
+    period: 60,
+    evaluationPeriods: 1,
+    comparisonOperator: 'GreaterThanThreshold',
+  },
+  functionThrottles: {
+    namespace: lambdaNamespace,
+    metric: 'Throttles',
+    threshold: 50,
+    statistic: 'Sum',
+    period: 60,
+    evaluationPeriods: 1,
+    comparisonOperator: 'GreaterThanThreshold',
+  }
 };
