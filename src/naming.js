27b7f4a0153456ec4fd994a4d7af29cfdf8ffdd9
John McKim <johncmckim@gmail.com>
2017-07-06 22:34:57 +1000

    Add editor config (#30)
    * Add editor config
    
    * Add issue templates

--- src/naming.js
+++ src/naming.js
@@ -3,25 +3,25 @@
 const _ = require('lodash');
 
 const getNormalisedName = (name) => {
-	return `${_.upperFirst(name.replace(/-/g, 'Dash').replace(/_/g, 'Underscore'))}`;
+  return `${_.upperFirst(name.replace(/-/g, 'Dash').replace(/_/g, 'Underscore'))}`;
 }
 
 class Naming {
 
-	getAlarmCloudFormationRef(alarmName, prefix) {
-		const normalizePrefix = getNormalisedName(prefix);
-		const normalizedName = getNormalisedName(alarmName);
+  getAlarmCloudFormationRef(alarmName, prefix) {
+    const normalizePrefix = getNormalisedName(prefix);
+    const normalizedName = getNormalisedName(alarmName);
 
-		return `${normalizePrefix}${normalizedName}Alarm`;
-	}
+    return `${normalizePrefix}${normalizedName}Alarm`;
+  }
 
-	getLogMetricCloudFormationRef(normalizedName, alarmName){
-		return `${normalizedName}${_.upperFirst(alarmName)}LogMetricFilter`;
-	}
+  getLogMetricCloudFormationRef(normalizedName, alarmName) {
+    return `${normalizedName}${_.upperFirst(alarmName)}LogMetricFilter`;
+  }
 
-	getPatternMetricName(metricName, functionName){
-		return `${_.upperFirst(metricName)}${functionName}`;
-	}
+  getPatternMetricName(metricName, functionName) {
+    return `${_.upperFirst(metricName)}${functionName}`;
+  }
 
 }
 
