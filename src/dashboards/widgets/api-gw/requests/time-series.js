2916ed69de0aa2997514f8eeed84e1533a8a8e25
Bryan Gamble <bdgamble@users.noreply.github.com>
2017-07-19 18:31:10 -0400

    Fix the metrics for api-gw latency and requests widgets, which were swapped (#39)

--- src/dashboards/widgets/api-gw/requests/time-series.js
+++ src/dashboards/widgets/api-gw/requests/time-series.js
@@ -14,10 +14,9 @@ const createWidget = (config) => {
       view: 'timeSeries',
       stacked: false,
       metrics: [
-          [ 'AWS/ApiGateway', 'IntegrationLatency', 'ApiName', apiName, { stat: 'p50', period: 900, region: config.region } ],
-          [ 'AWS/ApiGateway', 'Latency', 'ApiName', apiName, { stat: 'p50', period: 900, region: config.region } ],
-          [ 'AWS/ApiGateway', 'IntegrationLatency', 'ApiName', apiName, { stat: 'p90', period: 900, region: config.region } ],
-          [ 'AWS/ApiGateway', 'Latency', 'ApiName', apiName, { stat: 'p90', period: 900, region: config.region } ]
+          [ 'AWS/ApiGateway', '5XXError', 'ApiName', apiName, { stat: 'Sum', period: 900 } ],
+          [ 'AWS/ApiGateway', '4XXError', 'ApiName', apiName, { stat: 'Sum', period: 900 } ],
+          [ 'AWS/ApiGateway', 'Count', 'ApiName', apiName, { stat: 'Sum', period: 900 } ]
       ],
       region: config.region,
     }
